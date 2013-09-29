9385f61e30eb55beb0d6f9279d6bfad692b158cf
ThisguynamedSAM <Sam.d.colon@gmail.com>
2013-09-28 20:44:57 -0400

    add javascript and change routes

---
+++ app/assets/javascripts/fountains.js
@@ -0,0 +1,44 @@
+
+function success(position) {
+  console.log("Got a position");
+  var mapcanvas = document.createElement('div');
+  mapcanvas.id = 'mapcanvas';
+  mapcanvas.style.height = '400px';
+  mapcanvas.style.width = '560px';
+    
+  document.querySelector('article').appendChild(mapcanvas);
+  
+  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var myOptions = {
+    zoom: 15,
+    center: latlng,
+    mapTypeControl: false,
+    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+  
+  var marker = new google.maps.Marker({
+      position: latlng, 
+      map: map, 
+      title:"You are here! (at least within a "+position.coords.accuracy+" meter radius)"
+  });
+}
+
+function error(msg) {
+  var s = document.querySelector('#status');
+  s.innerHTML = typeof msg == 'string' ? msg : "failed";
+  s.className = 'fail';
+  
+  // console.log(arguments);
+}
+
+function getLocation(){
+	if (navigator.geolocation) {
+	  console.log("About to call the getCurrentPosition function");
+	  navigator.geolocation.getCurrentPosition(success, error);
+	} else {
+	  error('not supported');
+	}
+}
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Forfreeplease::Application.routes.draw do
-  get "fountains/index"
+  root to: "fountains/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
