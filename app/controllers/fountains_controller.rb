22ab3580fc4dea6dda69905d7bca8e81493bb790
Johan Mena <johan.menac@gmail.com>
2013-09-28 21:59:55 -0400

    Respond to the request only if it has all params This is to get around the page loading for the first time and not passing in the user location right away.

--- app/controllers/fountains_controller.rb
+++ app/controllers/fountains_controller.rb
@@ -1,4 +1,10 @@
 class FountainsController < ApplicationController
   def index
+    if params.size > 2
+      respond_to do |format|
+        format.html
+        format.json { render json: Fountain.near(params) }
+      end
+    end
   end
 end
