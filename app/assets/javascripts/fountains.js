8ad95775fae08a54717230e184e2831282589b99
Johan Mena <johan.menac@gmail.com>
2013-09-28 21:32:16 -0400

    Move some JS around

--- app/assets/javascripts/fountains.js
+++ app/assets/javascripts/fountains.js
@@ -1,43 +1,78 @@
-function success(position) {
-  console.log("Got a position");
-  var mapcanvas = document.createElement('div');
-  mapcanvas.id = 'mapcanvas';
-  mapcanvas.style.height = '400px';
-  mapcanvas.style.width = '560px';
-
-  console.log("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
-
-  document.querySelector('article').appendChild(mapcanvas);
-
-  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  var myOptions = {
-    zoom: 15,
-    center: latlng,
-    mapTypeControl: false,
-    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
-
-  var marker = new google.maps.Marker({
-      position: latlng, 
-      map: map, 
-      title:"You are here! (at least within a "+position.coords.accuracy+" meter radius)"
-  });
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, handleError, {maximumAge: 600000, timeout: 5000});
+    } else {
+        alert("Location cannot be retrieved");
+    }
 }
 
-function error(msg) {
-  var s = document.querySelector('#status');
-  s.innerHTML = typeof msg == 'string' ? msg : "failed";
-  s.className = 'fail';
+function handleError(error) {
+    console.log("There has been an error: " + error);
+    console.log("Probably the timeout expired?");
 }
 
-function getLocation(){
-  if (navigator.geolocation) {
-    console.log("About to call the getCurrentPosition function");
-    navigator.geolocation.getCurrentPosition(success, error, {maximumAge: 600000, timeout: 5000});
-  } else {
-    error('not supported');
-  }
+// Gets the radius specified by the user. Defaults to 0.5 km
+function getRadius() {
+    var radioButtons = document.getElementsByTagName('input');
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].type === 'radio' && radioButtons[i].checked) {
+            return parseFloat(radioButtons[i].value);
+        }
+    }
 }
 
+function success(userPosition) {
+    var lat = userPosition.coords.latitude;
+    var lon = userPosition.coords.longitude;
+    var rad = getRadius();
+
+    console.log("User is at: [" + lon + ", " + lat + "], and selected radius " + rad);
+
+    $.getJSON("fountains", { longitude: lon, latitude: lat, radius: rad }, function(data) {
+            showMap(data, userPosition);
+    });
+}
+
+function showMap(fountains, userPosition) {
+    // Should handle case where there's no fountains to display
+    var mapOptions = {
+        center: new google.maps.LatLng(userPosition.coords.latitude, userPosition.coords.longitude),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    createMarkersForMap(fountains, map);
+}
+
+function createMarkersForMap(fountains, map) {
+    var marker = new google.maps.Marker({
+        position: map.getCenter(),
+        map: map,
+        title: 'you'
+    });
+
+    var pinColor = "3E90FA";
+    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+        new google.maps.Size(21, 34),
+        new google.maps.Point(0,0),
+        new google.maps.Point(10, 34));
+    var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new google.maps.Size(40, 37),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(12, 35));
+
+    fountains.forEach(function(fountain) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(fountain.location[1], fountain.location[0]),
+            map: map,
+            icon: pinImage,
+            shadow: pinShadow
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            map.setZoom(15);
+            map.setCenter(marker.getPosition());
+        });
+    });
+}
