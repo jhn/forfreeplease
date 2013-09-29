9b2fccfb825150e277b67c070a805e649fe989c7
Johan Mena <johan.menac@gmail.com>
2013-09-28 21:01:08 -0400

    Add a basic method for the model

--- app/models/fountain.rb
+++ app/models/fountain.rb
@@ -4,4 +4,11 @@ class Fountain
   field :name, type: String
 
   index({location: '2dsphere'})
+
+  class << self
+    def near(params={})
+      # Should be an array containing [lat, lon]
+      # The radious is divided by
+    end
+  end
 end
