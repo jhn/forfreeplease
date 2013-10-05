58ebf1af3cd5cca81a2fca54c8f43f1fd042f5ec
Johan Mena <johan.menac@gmail.com>
2013-10-05 02:19:41 -0400

    Write parsers. Work in progress.

---
+++ lib/wifi_parser.rb
@@ -0,0 +1,34 @@
+# wifi: id, name, address, city, url, phone, type, zip
+# fountains: id, lat, lon, name
+#
+# Instead I can create a method that takes in a list of headers
+# and returns the data parsed
+require './base_parser'
+
+class WifiParser < BaseParser
+end
+
+#I can't call split on array here.
+wifi = WifiParser.new
+res = wifi.each_record("wifi.csv") do |row|
+  id, name, address, city, url, phone, type, zip = row.split(',')
+  row = {}
+  row[:id] = id
+  row[:name] = name
+  row[:address] = address
+  row[:city] = city
+  row[:url] = url
+  row[:phone] = phone
+  row[:type] = type
+  row[:zip] = zip
+  row
+end
+
+puts res.inspect
+# require 'csv'
+
+# result = {}
+# CSV.foreach("wifi.csv") do |row|
+#   id, name, address, city, url, phone, type, zip = row.split(',')
+#   result
+# end
