0fc97f6aaee35da63484ffef7e262cada4c6c513
Johan Mena <johan.menac@gmail.com>
2013-12-23 10:47:08 -0500

    Refactor long foursquare deals method

--- app/controllers/fountains_controller.rb
+++ app/controllers/fountains_controller.rb
@@ -9,17 +9,22 @@ class FountainsController < ApplicationController
   end
 
   def foursquare
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    radius = params[:radius].to_f * 1609.34
+    respond_to do |format|
+      format.html
+      format.json { render json: specials_for_location(params) }
+    end
+  end
+
+  private
+
+  def specials_for_location(params)
     client = Foursquare2::Client.new(
       client_id: ENV['FOURSQUARE_ID'],
       client_secret: ENV['FOURSQUARE_SECRET']
     )
-    specials = client.search_specials(ll:"#{latitude},#{longitude}", radius: radius)
-    respond_to do |format|
-      format.html
-      format.json { render json: specials }
-    end
+    client.search_specials(
+      ll:"#{params[:latitude]},#{params[:longitude]}",
+      radius: params[:radius].to_f * 1609.34
+    )
   end
 end
